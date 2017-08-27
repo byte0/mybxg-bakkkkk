95c9cf654d13e24fcaa19d69c774666812d74e46
code1031 <wangzijie1031@qq.com>
2017-08-27 22:04:03 +0800

    完成登录退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,24 @@
 
-	NProgress.start();
+NProgress.start();
+NProgress.done();
 
-	NProgress.done();
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
-	});
+// 实现退出功能
+$('#logoutBtn').click(function(){
+    console.log(123);
+    $.ajax({
+        type : 'post',
+        url : '/api/logout',
+        dataType : 'json',
+        success : function(data){
+            if(data.code == 200){
+                // 退出成功
+                location.href = '/main/login';
+            }
+        }
+    });
+    return false;
+});
