546a8d30d884031c9cdaf34b9a5c0ce2c453b643
code1031 <wangzijie1031@qq.com>
2017-08-30 10:25:17 +0800

    讲师列表展示功能

--- public/js/config.js
+++ public/js/config.js
@@ -4,7 +4,8 @@ require.config({
     jquery : 'jquery/jquery.min',
     cookie : 'jquery-cookie/jquery.cookie',
     template : 'artTemplate/template-web',
+    common : '../js/common',
     login : '../js/login',
-    common : '../js/common'
+    teacherlist : '../js/teacher-list'
   }
 });
