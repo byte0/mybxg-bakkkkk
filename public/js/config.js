d8e0c44abdc6e6e1a18779fecf011c59c0ad69cd
code1031 <wangzijie1031@qq.com>
2017-08-30 11:03:41 +0800

    完成讲师预览功能

--- public/js/config.js
+++ public/js/config.js
@@ -4,8 +4,14 @@ require.config({
     jquery : 'jquery/jquery.min',
     cookie : 'jquery-cookie/jquery.cookie',
     template : 'artTemplate/template-web',
+    bootstrap : 'bootstrap/js/bootstrap.min',
     common : '../js/common',
     login : '../js/login',
     teacherlist : '../js/teacher-list'
+  },
+  shim : { // 把非标准模块转化为标准模块
+    bootstrap : {
+      deps : ['jquery']
+    }
   }
 });
--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -1,4 +1,4 @@
-define(['jquery','template'],function($,template){
+define(['jquery','template','bootstrap'],function($,template){
   // 请求后台接口获取列表数据
   $.ajax({
     type : 'get',
@@ -8,6 +8,27 @@ define(['jquery','template'],function($,template){
       // 解析数据并渲染页面
       var html = template('teacherTpl',{list : data.result});
       $('#teacherInfo').html(html);
+      // 绑定预览点击事件
+      $('.preveiw').click(function(){
+        // 通过接口获取数据
+        var tcId = $(this).closest('td').attr('data-tcId');
+        $.ajax({
+          type : 'get',
+          url : '/api/teacher/view',
+          data : {tc_id:tcId},
+          dataType : 'json',
+          success : function(data){
+            // 解析数据渲染页面
+            var html = template('modalTpl',data.result);
+            $('#modalInfo').html(html);
+            // 显示弹窗
+            $('#teacherModal').modal();
+          }
+        });
+
+      });
     }
   });
+
+
 });
