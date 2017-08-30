a62461152e0f085a2d56308de09903d36ee82f1e
code1031 <wangzijie1031@qq.com>
2017-08-30 11:48:31 +0800

    完成启用注销讲师功能

--- public/js/teacher-list.js
+++ public/js/teacher-list.js
@@ -25,7 +25,27 @@ define(['jquery','template','bootstrap'],function($,template){
             $('#teacherModal').modal();
           }
         });
-
+      });
+      // 处理启用注销功能
+      $('.eod').click(function(){
+        var td = $(this).closest('td');
+        var tcId = td.attr('data-tcId');
+        var tcStatus = td.attr('data-status');
+        var that = this;// 点击的按钮
+        $.ajax({
+          type : 'post',
+          url : '/api/teacher/handle',
+          data : {tc_id : tcId,tc_status : tcStatus},
+          dataType : 'json',
+          success : function(data){
+            td.attr('data-status',data.result.tc_status);
+            if(data.result.tc_status == 0){
+              $(that).html('注 销');
+            }else{
+              $(that).html('启 用');
+            }
+          }
+        });
       });
     }
   });
