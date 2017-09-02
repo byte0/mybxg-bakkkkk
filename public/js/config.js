3c40aa7d96e0519d909c4055543e7ef2f00c8790
code1031 <wangzijie1031@qq.com>
2017-09-02 10:25:30 +0800

    完成头像上传功能

--- public/js/config.js
+++ public/js/config.js
@@ -9,6 +9,7 @@ require.config({
     language : 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
     validate : 'validate/jquery-validate.min',
     form : 'jquery-form/jquery.form',
+    uploadify : 'uploadify/jquery.uploadify.min',
     common : '../js/common',
     login : '../js/login',
     index : '../js/index',
@@ -25,6 +26,9 @@ require.config({
       deps : ['jquery','datepicker']
     },
     validate : {
+      deps : ['jquery']
+    },
+    uploadify : {
       deps : ['jquery']
     }
   }
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','datepicker','language','uploadify'],function($,template,util){
   // 设置导航菜单选中
   util.setMenu('/main/index');
   // 调用后台接口获取所有的个人信息
@@ -10,7 +10,21 @@ define(['jquery','template','util'],function($,template,util){
       // 解析数据渲染页面
       var html = template('settingsTpl',data.result);
       $('#settingsInfo').html(html);
-
+      // 处理头像上传
+      $('#upfile').uploadify({
+        width : 120,
+        height : 120,
+        buttonText : '',
+        itemTemplate : '<span></span>',
+        fileObjName : 'tc_avatar',
+        swf : '/public/assets/uploadify/uploadify.swf',
+        uploader : '/api/uploader/avatar',
+        onUploadSuccess : function(f,data){
+          var data = JSON.parse(data);
+          // 修改图片的URL地址
+          $('.preview img').attr('src',data.result.path);
+        }
+      });
     }
   });
 });
