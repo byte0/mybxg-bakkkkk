f3face8eac65eacfa60674f8d935414bd5da3e1f
code1031 <wangzijie1031@qq.com>
2017-09-03 12:18:50 +0800

    实现课程封面上传操作

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,4 @@
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','uploadify'],function($,template,util){
   // 设置导航菜单
   util.setMenu('/course/add');
   // 获取课程ID
@@ -13,6 +13,23 @@ define(['jquery','template','util'],function($,template,util){
       // 渲染页面
       var html = template('pictureTpl',data.result);
       $('#pictureInfo').html(html);
+      // 处理课程封面的上传
+      $('#upfile').uploadify({
+        width : 80,
+        height :30,
+        buttonText : '选择图片',
+        buttonClass : 'btn btn-success btn-sm upfilebtn',
+        itemTemplate : '<span></span>',
+        fileObjName : 'cs_cover_original',
+        formData : {cs_id : csId},
+        swf : '/public/assets/uploadify/uploadify.swf',
+        uploader : '/api/uploader/cover',
+        onUploadSuccess : function(f,data){
+          var data = JSON.parse(data);
+          // 修改图片的URL地址
+          $('.preview img').attr('src',data.result.path);
+        }
+      });
     }
   });
 });
