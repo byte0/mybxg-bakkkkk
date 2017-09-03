b030c2d01eb6d479012128edbb73d905b11de39c
code1031 <wangzijie1031@qq.com>
2017-09-03 11:57:01 +0800

    实现课程封面展示功能

--- public/js/course-picture.js
+++ public/js/course-picture.js
@@ -1,4 +1,18 @@
 define(['jquery','template','util'],function($,template,util){
   // 设置导航菜单
   util.setMenu('/course/add');
+  // 获取课程ID
+  var csId = util.qs('cs_id');
+  // 查询课程封面信息
+  $.ajax({
+    type : 'get',
+    url : '/api/course/picture',
+    data : {cs_id : csId},
+    dataType : 'json',
+    success : function(data){
+      // 渲染页面
+      var html = template('pictureTpl',data.result);
+      $('#pictureInfo').html(html);
+    }
+  });
 });
